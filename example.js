56473a73003fe7d7ac2e8f765f02fa26b946db01
musicslayer <musicslayer@gmail.com>
2023-08-14 15:24:46 -0400

    Initial Submission
    Initial Submission

---
+++ example.js
@@ -0,0 +1,10 @@
+const path = require("path");
+const Zip = require("./js/Zip.js");
+
+const ZIP_SOURCE_FOLDER = path.resolve(path.join("example_zip_files", "files"));
+const ZIP_FILE_PATH = path.resolve(path.join("example_zip_files", "file.zip"));
+
+async function init() {
+    await Zip.createZipFileFromFolder(ZIP_FILE_PATH, ZIP_SOURCE_FOLDER);
+}
+init();
